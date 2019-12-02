4655375a676b44d9237392f36c6985e275896322
Conan Mafnas <conan@Conans-iMac.lan>
2019-12-01 23:30:07 -0500

    updating production environment

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ server.listen(PORT_NUMBER, () => {
     console.log(`Server is running on port ${PORT_NUMBER}`)
 })
 
-server.use(express.static('build'));
+server.use(express.static('/build/build'));
 server.get('*', (req, res) => {
-res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+res.sendFile(path.resolve(__dirname, 'build', 'build', 'index.html'));
 });
 
-server.get("/express_backend", (req, res) => {
-  res.json([{ id: 1, express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" }]);
-});
+// server.get("/express_backend", (req, res) => {
+//   res.json([{ id: 1, express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" }]);
+// });
+
+console.log(path.resolve(__dirname, "build", "build", "index.html"));
 
 
 
